e10bdab077e38c5807598b6c7326f03c219eb5b0
Rachnera <arachne.silk@gmail.com>
2024-01-25 20:36:18 +0100

    Setup consistent default values

--- Rachnera Keybind Config.rb
+++ Rachnera Keybind Config.rb
@@ -1,3 +1,15 @@
+### Default values ###
+
+# Use VX Ace standard default values as default
+# Ref: https://forums.rpgmakerweb.com/index.php?threads/rpg-maker-pc-game-controls-mv-vx-ace-vx-xp-2003-2000.140758/
+System::Defaults[:p1][:f_up] = System::Defaults[:p1][:m_up] = [:UP, :NUMPAD8]
+System::Defaults[:p1][:f_down] = System::Defaults[:p1][:m_down] = [:DOWN, :NUMPAD2]
+System::Defaults[:p1][:f_left] = System::Defaults[:p1][:m_left] = [:LEFT, :NUMPAD4]
+System::Defaults[:p1][:f_right] = System::Defaults[:p1][:m_right] = [:RIGHT, :NUMPAD6]
+System::Defaults[:p1][:f_confirm] = System::Defaults[:p1][:m_confirm] = [:RETURN, :SPACE]
+System::Defaults[:p1][:f_cancel] = System::Defaults[:p1][:m_cancel] = System::Defaults[:p1][:m_menu] = [:ESCAPE, :LETTER_X, :NUMPAD0]
+System::Defaults[:p1][:m_pgdown] = [:LETTER_W,:NEXT]
+
 # Save the config in a file instead of having it been lost every time the game is closed
 System::Reload = false
 
@@ -33,3 +45,9 @@ System::Defaults[:system][:debug] = []
 ConfigScene::Categs[:system][:list].delete(:debug)
 System::Defaults[:p1][:d_through] = []
 ConfigScene::Categs[:p1_map][:list].delete(:d_through)
+
+# Allow up to 3 different keys to be bound to a feature
+ConfigScene::Categs.each_key do |key|
+  ConfigScene::Categs[key][:max] = 3
+end
+ConfigScene::ListVisual[:padding] = 100
